b0e772323f68b446d054a87cc4815b17616fdb9b
unknown <saraszewczykk@wp.pl>
2023-03-01 13:05:43 +0100

    New files added

---
+++ admin.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Panel administracyjny</title>
+    <meta name="description" content="Używanie PDO - odczyt z bazy MySQL">
+    <meta name="keywords" content="php, kurs, PDO, połączenie, MySQL">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <div class="container">
+
+        <header>
+            <h1>Admin</h1>
+        </header>
+
+        <main>
+            <article>
+                <form method="post" action="list.php">
+                    <label>Login <input type="text" name="login"></label>
+                    <label>Hasło <input type="password" name="pass"></label>
+                    <input type="submit" value="Zaloguj się!">
+                </form>
+            </article>
+        </main>
+
+    </div>
+</body>
+</html>
---
+++ list.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Panel administracyjny</title>
+    <meta name="description" content="Używanie PDO - odczyt z bazy MySQL">
+    <meta name="keywords" content="php, kurs, PDO, połączenie, MySQL">
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <div class="container">
+
+        <header>
+            <h1>Newsletter</h1>
+        </header>
+
+        <main>
+            <article>
+  
+            </article>
+        </main>
+
+    </div>
+
+</body>
+</html>
