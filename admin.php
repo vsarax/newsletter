5b0d71b06fbe29836cb137a89d4c71b20dd7e0af
unknown <saraszewczykk@wp.pl>
2023-03-02 13:04:10 +0100

    admin log in set, login and password validation

--- admin.php
+++ admin.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (isset($_SESSION['logged_id'])) {
+    header('Location: list.php');
+    exit();
+}
 
 ?>
 <!DOCTYPE html>
@@ -10,12 +14,8 @@ session_start();
     <meta name="description" content="Używanie PDO - odczyt z bazy MySQL">
     <meta name="keywords" content="php, kurs, PDO, połączenie, MySQL">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
     <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-    <!--[if lt IE 9]>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <![endif]-->
 </head>
 
 <body>
@@ -31,6 +31,12 @@ session_start();
                     <label>Login <input type="text" name="login"></label>
                     <label>Hasło <input type="password" name="pass"></label>
                     <input type="submit" value="Zaloguj się!">
+                    <?php
+                    if (isset($_SESSION['wrong_attempt'])) {
+                        echo '<p>Niepoprawny login lub hasło!</p>';
+                        unset($_SESSION['wrong_attempt']);
+                    }
+                    ?>
                 </form>
             </article>
         </main>
--- index.php
+++ index.php
@@ -9,12 +9,8 @@ session_start();
     <meta name="description" content="Używanie PDO - zapis do bazy MySQL">
     <meta name="keywords" content="php, kurs, PDO, połączenie, MySQL">
     <meta http-equiv="X-Ua-Compatible" content="IE=edge">
-
     <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-    <!--[if lt IE 9]>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <![endif]-->
 </head>
 
 <body>
